2545750f59e721a9351c101aade59e1201d60c33
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-01-07 21:32:43 +0900

    Change amdaemon flag for mai2

--- dist/mai2/start.bat
+++ dist/mai2/start.bat
@@ -2,7 +2,7 @@
 
 pushd %~dp0
 
-start /min inject -d -k mai2hook.dll amdaemon.exe -c config_common.json config_server.json config_client.json
+start /min inject -d -k mai2hook.dll amdaemon.exe -f -c config_common.json config_server.json config_client.json
 inject -d -k mai2hook.dll sinmai -screen-fullscreen 0
 taskkill /f /im amdaemon.exe > nul 2>&1
 
