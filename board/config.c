a53515634a0d8fb111f9d28e6a4b90a94a98e40a
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-04-10 16:15:36 +0900

    aimeio: use different aimeio path for each env

--- board/config.c
+++ board/config.c
@@ -9,18 +9,52 @@
 #include "board/config.h"
 #include "board/sg-reader.h"
 
+// Check windows
+#if _WIN32 || _WIN64
+   #if _WIN64
+     #define ENV64BIT
+  #else
+    #define ENV32BIT
+  #endif
+#endif
+
+// Check GCC
+#if __GNUC__
+  #if __x86_64__ || __ppc64__
+    #define ENV64BIT
+  #else
+    #define ENV32BIT
+  #endif
+#endif
+
 static void aime_dll_config_load(struct aime_dll_config *cfg, const wchar_t *filename)
 {
     assert(cfg != NULL);
     assert(filename != NULL);
+    
+    // Workaround for x64/x86 external aime IO dlls
+    // path32 for 32bit, path64 for 64bit
 
-    GetPrivateProfileStringW(
-            L"aimeio",
-            L"path",
-            L"",
-            cfg->path,
-            _countof(cfg->path),
-            filename);
+    #if defined(ENV32BIT)
+        GetPrivateProfileStringW(
+                L"aimeio",
+                L"path32",
+                L"",
+                cfg->path,
+                _countof(cfg->path),
+                filename);
+    #elif defined(ENV64BIT)
+        GetPrivateProfileStringW(
+                L"aimeio",
+                L"path64",
+                L"",
+                cfg->path,
+                _countof(cfg->path),
+                filename);
+    #else
+        #error "Unknown environment"
+    #endif
+
 }
 
 void aime_config_load(struct aime_config *cfg, const wchar_t *filename)
